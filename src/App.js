2f4b921c9f041ab08e4736d1ad007bc9a7976567
AiDroopy <AiDroopy@users.noreply.github.com>
2022-03-21 15:03:24 +0100

    Added BrowserRouter to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { AuctionProvider } from './context/AuctionContext';
 
 function App() {
   return (
     <div className="App">
+    <AuctionProvider>
       <header className="App-header">
        
       </header>
+      </AuctionProvider>  
     </div>
   );
 }
---
+++ src/components/TestCont.js
@@ -0,0 +1,7 @@
+const TestCont = () => {
+    return ( <div>
+        
+    </div> );
+}
+ 
+export default TestCont;
